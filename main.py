d2c10e5f507fc2514e15b7e2bc4633a8162a8336
SalazarV4 <diegosalazar.v4@gmail.com>
2025-08-12 12:28:07 -0500

    Modified main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from argparse import ArgumentParser
 import dagshub
 from kidney_cancer import logger
 from kidney_cancer.pipeline.data_ingestion import DataIngestionTrainingPipeline
@@ -6,7 +5,7 @@ from kidney_cancer.pipeline.base_model import BaseModelTrainingPipeline
 from kidney_cancer.pipeline.model_training import ModelTrainingPipeline
 from kidney_cancer.pipeline.model_evaluation import ModelEvaluationPipeline
 
-dagshub.init(repo_owner='SalazarV4', repo_name='Kidney_cancer', mlflow=True)
+#dagshub.init(repo_owner='SalazarV4', repo_name='Kidney_cancer', mlflow=True)
 
 STAGE_NAME_1 = "Data Ingestion"
 try:
